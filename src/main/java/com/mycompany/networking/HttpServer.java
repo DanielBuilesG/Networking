0643a8088d765930654af22ebe1b14ee1661c0b5
DanielBuilesG <daniel.builes99@gmail.com>
2023-06-21 12:27:18 -0500

    add of 'www' folder, add index.html file, completion of httpServer behaviour

--- src/main/java/com/mycompany/networking/HttpServer.java
+++ src/main/java/com/mycompany/networking/HttpServer.java
@@ -10,6 +10,11 @@ package com.mycompany.networking;
  */
 import java.net.*;
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.File;
+import java.nio.file.Files;
 
 public class HttpServer {
 
@@ -39,16 +44,65 @@ public class HttpServer {
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
             String inputLine, outputLine;
-
+                
+            boolean firstReqLine = true;
+            String request = "";
+            
             while ((inputLine = in.readLine()) != null) {
                 System.out.println("Received: " + inputLine);
-
+                if (firstReqLine){
+                    firstReqLine = false;
+                    request = inputLine;
+                }
                 if (!in.ready()) {
                     break;
                 }
             }
+            
+            outputLine = defaultResponse();
+            out.println(outputLine);
+            createResponse(request, out);
+            
 
-            outputLine = "HTTP/1.1 200 OK\r\n"
+            out.close();
+            in.close();
+            clientSocket.close();
+            
+        }
+        serverSocket.close();
+    }
+    
+    private static void createResponse(String request, PrintWriter out) {
+        //GET /index.html HTTP/1.1
+        System.out.println("Request interpret: " + request);
+        if(request.equals("")){return;}
+        String[] tokenizedRequest = request.split(" ");
+        
+        String httpVerb = tokenizedRequest[0];
+        String path = tokenizedRequest[1];
+        String protocol = tokenizedRequest[2];
+        
+        Path file= Paths.get("./www" + path);
+        
+        String defaultHeader ="HTTP/1.1 200 OK\r\n"
+                    + "Content-Type: text/html\r\n"
+                    + "\r\n";
+        
+        Charset charset = Charset.forName("UTF-8");
+        out.println(defaultHeader);
+        try(BufferedReader reader = Files.newBufferedReader(file, charset)){
+            String line = null;
+            while ((line = reader.readLine()) != null){
+                System.out.println(line);
+                out.println(line);
+            }
+        }catch(IOException x){
+            System.err.format("IOExeception: %s%n", x);
+        }
+    }
+    
+    public static String defaultResponse(){
+        return "HTTP/1.1 200 OK\r\n"
                     + "Content-Type: text/html\r\n"
                     + "\r\n"
                     + "<!DOCTYPE html>"
@@ -60,15 +114,10 @@ public class HttpServer {
                     + "<body>"
                     + "My Web Site"
                     + "</body>"
-                    + "</html>";
+                    + "</html>"; 
 
-            out.println(outputLine);
-
-            out.close();
-            in.close();
-            clientSocket.close();
             
-        }
-        serverSocket.close();
     }
+
+    
 }
