b194e65ca1c3e98779519181ae06445ecdbd0238
DanielBuilesG <daniel.builes99@gmail.com>
2023-06-21 09:17:19 -0500

    primer commit

---
+++ src/main/java/com/mycompany/networking/Networking.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.networking;
+
+/**
+ *
+ * @author daniel.builes
+ */
+public class Networking {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
