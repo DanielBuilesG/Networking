063121eb538d504ab2dfcf90c0fff41b71be9ca5
DanielBuilesG <daniel.builes99@gmail.com>
2023-06-22 10:04:06 -0500

    fixed friend class

--- src/main/java/com/mycompany/concurrencia/Friend.java
+++ src/main/java/com/mycompany/concurrencia/Friend.java
@@ -11,6 +11,8 @@ package com.mycompany.concurrencia;
 public class Friend {
     
     private final String name;
+    private final Object lock1 = new Object();
+    private final Object lock2 = new Object();
     
     public Friend(String name){
         this.name = name;
@@ -21,20 +23,32 @@ public class Friend {
         return this.name;
     }
     
-    public synchronized void bow(Friend bower){
-        System.out.format("%s: %s" + " has bowed to me!%n", this.name, bower.getName());
-        bower.bowBack(this);
+    public void bow(Friend bower){
+        synchronized (lock1){
+            System.out.format("%s: %s" + " has bowed to me!%n", this.name, bower.getName());
+            bower.bowBack(this);
+        }
     }
-    public synchronized void bowBack(Friend bower){
-        System.out.format("%s: %s" + " has bowed to me!%n", this.name, bower.getName());
+    public void bowBack(Friend bower){
+        synchronized (lock2){
+            System.out.format("%s: %s" + " has bowed back to me!%n", this.name, bower.getName());
+        }
     }
     
     public static void main(String[] args){
         final Friend alphonse = new Friend("Alphonse");
         final Friend gaston = new Friend("Gaston");
+        
+        
         new Thread(new Runnable(){
             public void run(){
                 gaston.bow(alphonse);
+            }
+        }).start();
+        
+        new Thread(new Runnable(){
+            public void run(){
+                alphonse.bow(gaston);
             }
         }).start();
     }
